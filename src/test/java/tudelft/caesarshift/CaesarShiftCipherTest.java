809ca1a22059550a28837ca03a919e2658671a48
harshdevda <harsh.devda@gmail.com>
2021-02-24 14:51:39 +0530

    Fixed CaesarShiftCipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,26 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
+    @Test
+    public void shiftCharGreaterthan26(){
+        CaesarShiftCipher str1= new CaesarShiftCipher();
+        String result= str1.CaesarShiftCipher("abc",25);
+        Assertions.assertEquals("zab",result);
+    }
+    @Test
+    public void shiftCharLessthan26(){
+        CaesarShiftCipher str1= new CaesarShiftCipher();
+        String result= str1.CaesarShiftCipher("abc",5);
+        Assertions.assertEquals("fgh",result);
+    }
+    @Test
+    public void shiftCharlessthan0(){
+        CaesarShiftCipher str1= new CaesarShiftCipher();
+        String result= str1.CaesarShiftCipher("abc",-2);
+        Assertions.assertEquals("yza",result);
+    }
 
 }
